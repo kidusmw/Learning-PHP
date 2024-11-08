64a2f080cbc969514729ebecd722bcf561b5fe60
Kidus Maru <kiduswubie7110@gmail.com>
2024-11-08 07:50:02 +0300

    Add Feature on home.php and Add Logout.php

--- home.php
+++ home.php
@@ -1,4 +1,12 @@
 <?php
 
 session_start();
-echo $_SESSION['user'];
+if (isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
+} else {
+    header('location:code.php');
+}
+echo $user
+
+?>
+<a href="logout.php">Log out</a>
